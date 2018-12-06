0e4bcb18bbbb4cc1d54d0e94a2f943eb18e7286a
Flotuz <flotuz10@gmail.com>
2018-12-06 12:05:38 -0600

    Login

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,38 @@ import java.util.Scanner;
 public class Main {
 private static ArrayList <Habitacion> habs =  new ArrayList<>();
 private static Scanner teclado = new Scanner(System.in);
+private static Scanner leer;
     public static void main(String[] args) throws FileNotFoundException {
         menu();
     }
 
     private static void menu() throws FileNotFoundException {
-
+        teclado = new Scanner(System.in);
         llenadoDeBase();
         System.out.printf("1 Tipo de habitaciones");
+        System.out.printf(" 2 Ingresa   ");
+        System.out.printf("9 Salir");
         int opcion = teclado.nextInt();
         switch (opcion){
-            case 1: tiposDeHabitaciones();
+            case 1: tiposDeHabitaciones();  break;
+            case 2: login();                break;
+            case 9 :                        break;
         }
+    }
+
+    private static boolean login() throws FileNotFoundException {
+        boolean usuario;
+        File login = new File("login.dat");
+        String correo, password;
+        System.out.printf("Ingrese correo");
+            correo = teclado.next();
+        System.out.printf("Ingrese ocntraseña");
+            correo = teclado.next();
+
+        if (true) {
+
+        }
+        return false;
     }
 
     private static void tiposDeHabitaciones() {
@@ -31,7 +51,7 @@ private static Scanner teclado = new Scanner(System.in);
 
     public static void llenadoDeBase() throws FileNotFoundException {
         File habitaciones=new File("Habitacion.txt");
-        Scanner leer=new Scanner(habitaciones);
+        leer=new Scanner(habitaciones);
 
         while (leer.hasNext()){
             String tipo=leer.next();
